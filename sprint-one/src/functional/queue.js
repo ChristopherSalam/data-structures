699927a40c882844fa54f89ca3aa4cc621dce115
ceditoph <CR.patterson2000@gmail.com>
2015-07-01 14:52:52 -0700

    queue only last test failing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,21 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value
+    size++
   };
 
   someInstance.dequeue = function(){
+    if(size>0){
+      size--
+      var value = storage[size]
+      delete storage[size]
+      return value
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
