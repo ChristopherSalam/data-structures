43fae6ce28483cc07bfd14ceca04731a24e42e71
ceditoph <CR.patterson2000@gmail.com>
2015-07-01 14:44:11 -0700

    functional stack working

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function(){
+  var size = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,12 +7,22 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size]=value
+    size++
   };
 
   someInstance.pop = function(){
+    if(size>0){
+      //not so intuitive, but reduces the length of the collection, then removes the last key of the collection
+      size--
+      var resultOfPop = storage[size];
+      delete storage[size]
+      return resultOfPop
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
